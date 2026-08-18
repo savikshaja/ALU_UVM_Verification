8aab9723d193ba7ef39c6a9fe3e66848696ae332
savikshaja <savikshaja@gmail.com>
2026-08-18 10:14:35 +0530

    initial

---
+++ src/test_bench/sequencer.sv
@@ -0,0 +1,8 @@
+class input_sequencer extends uvm_sequencer#(seq_item);
+	`uvm_component_utils(input_sequencer)
+
+ function new(string name="input_sequencer",uvm_component parent);
+	super.new(name,parent);
+ endfunction
+
+endclass
